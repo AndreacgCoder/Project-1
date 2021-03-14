5e49d6c2812941240d281e049dabdc55094fe53a
Andrea Comabasosa Garrido <comabasosa.g.a@gmail.com>
2021-03-14 20:21:19 +0100

    Borro codigo IF

--- button.js
+++ button.js
@@ -3,10 +3,5 @@ const menuBtn = document.querySelector(".menuIcon");
 const menu = document.querySelector(".navigation ul");
 
 menuBtn.addEventListener("click", function () {
-  menu.classList.toggle('show'); //Hace lo mismo que el if:
-  /*if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
-  } else {
-    menu.classList.add("show");
-  }*/
+  menu.classList.toggle('show'); 
 });
